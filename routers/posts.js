485ca9364c76e6f688a19d705cd4079cc533926e
KevinZucca <kev.94@live.it>
2023-11-24 17:53:47 +0100

    fix: refactoring of validations

--- routers/posts.js
+++ routers/posts.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts");
 const validator = require("../middlewares/validator");
-const idValidation = require("../validations/postsID");
-const titleValidation = require("../validations/postsTitle");
+const { paramID, bodyControl } = require("../validations/posts");
 
 router.get("/", postsController.index);
-router.post("/", validator(titleValidation), postsController.create);
-router.get("/:slug", validator(idValidation), postsController.show);
-router.put("/:slug", validator(idValidation), postsController.update);
-router.delete("/:slug", validator(idValidation), postsController.destroy);
+router.post("/", validator(bodyControl), postsController.create);
+router.get("/:slug", validator(paramID), postsController.show);
+router.put("/:slug", validator(paramID), postsController.update);
+router.delete("/:slug", validator(paramID), postsController.destroy);
 
 module.exports = router;
---
+++ validations/posts.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import("express-validator").Schema}
+ */
+const paramID = {
+  id: {
+    in: ["params"],
+    isInt: {
+      errorMessage: "ID must be an integer number",
+    },
+  },
+};
+
+/**
+ * @type {import("express-validator").Schema}
+ */
+const bodyControl = {
+  title: {
+    isString: {
+      errorMessage: "Title must be a string",
+    },
+    isLength: {
+      errorMessage: "Title must have minimum 5 characters",
+      options: { min: 5 },
+    },
+  },
+};
+
+module.exports = { paramID, bodyControl };
